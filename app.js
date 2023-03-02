02b75f2e20b97e4dc0c94af684e8c2c78c9ec34f
ablesense-hugh <hugh@ablesense.com>
2023-03-02 15:46:31 -0500

    add note function

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
-const command = process.argv[2]
+const yargs = require('yargs')
+const notes = require('./notes')
 
-if (command === 'add') {
-    console.log('Add notes!')
-} else if (command === 'remove') {
-    console.log('Remove notes!')
-} else if (command === 'read') {
-    console.log('Read notes!')
-} else if (command === 'list') {
-    console.log('List notes!')
-}
+//Add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: "string"
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler: function(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+//Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'remove a new note',
+    handler: function() {
+        console.log('Removing notes!')
+    }
+})
+
+//Read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a new note',
+    handler: function() {
+        console.log('Reading notes!')
+    }
+})
+
+//List command
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function() {
+        console.log('Listing notes!')
+    }
+})
+
+yargs.parse();
